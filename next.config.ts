9aa3474d9f085ca3a21ba9e6ca67bb26aad0c57e
Yuki <t.yuki112828@gmail.com>
2025-07-27 21:44:05 +0900

    2回目のコミット

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     domains: ['bjphqmzlirudxzvfpnye.supabase.co'],
   },
-};
-
-module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-}
+};
 
 export default nextConfig;
